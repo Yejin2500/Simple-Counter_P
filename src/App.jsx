c5cad49358034fe681e82ca6096b0bead8882a43
yejin <wkddpwls0115.naver.com>
2024-09-23 21:20:26 +0900

    [수정]

--- src/App.jsx
+++ src/App.jsx
@@ -7,9 +7,7 @@ function App() {
   const [count, setCount] = useState(0);
   const [input, setInput] = useState("");
 
-  useEffect(() => {
-    console.log('count: ${count} / input: ${input}');
-  }, [count, input]);
+  
 
   const onClickButton = (value) => {
     setCount(count + value);
